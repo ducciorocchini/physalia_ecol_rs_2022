27eeef6bea09600d396dcb92c441c4ff60c08872
ducciorocchini <ducciorocchini@gmail.com>
2022-09-05 17:17:03 +0200

    Create R_code_intro.r

---
+++ R_code_intro.r
@@ -0,0 +1,27 @@
+# Course at Physalia
+# https://www.physalia-courses.org/courses-workshops/remote-sensing-in-r/
+
+# INTRODUCTION TO ECOLOGICAL REMOTE SENSING IN R
+
+# Libraries needed:
+# raster
+# rgdal
+# ncdf4
+# RStoolbox
+# ggplot2
+# patchwork
+# rasterdiv
+# viridis
+
+# How to install them, e,g,:
+
+install.packages("raster")
+
+# then choose the CRAN, i.e. the city from the Comprehensive R Archive Network (any of them is ok!)
+
+# To check if the package is in, simply type:
+
+library(raster)
+
+# If nothing happens, that's ok! If you receive an error, write to:
+# Elisa Thouverai <elisa.thouverai2@unibo.it>
